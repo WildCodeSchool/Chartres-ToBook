768c090213919a7b573b9026be95fc37a78252d6
etidimjav <thomas.javanaud@gmail.com>
2016-07-01 11:30:18 +0200

    MAJ formulaire inscription

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -19,9 +19,69 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="user_prenom", type="string", nullable=true, length=128)
+     */
+    private $userPrenom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="user_prof", type="integer")
+     */
+    private $userProf;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Set userPrenom
+     *
+     * @param string $userPrenom
+     * @return User
+     */
+    public function setUserPrenom($userPrenom)
+    {
+        $this->userPrenom = $userPrenom;
+
+        return $this;
+    }
+
+    /**
+     * Get userPrenom
+     *
+     * @return string 
+     */
+    public function getUserPrenom()
+    {
+        return $this->userPrenom;
+    }
+
+    /**
+     * Set userProf
+     *
+     * @param integer $userProf
+     * @return User
+     */
+    public function setUserProf($userProf)
+    {
+        $this->userProf = $userProf;
+
+        return $this;
+    }
+
+    /**
+     * Get userProf
+     *
+     * @return integer 
+     */
+    public function getUserProf()
+    {
+        return $this->userProf;
     }
 }
