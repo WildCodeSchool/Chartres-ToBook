4671ebdaf44c49c81785a045e24731dc2d8a4097
topdeveloppement <topdeveloppement@gmail.com>
2016-07-08 10:33:52 +0200

    recupe bdd

--- web/script/script.js
+++ web/script/script.js
@@ -1,10 +1,9 @@
-/*
-* author Kennouche Omar . 
-*/
 var autocomplete;
 
 function initautocomplete() {
  
+  geolocate();
+
   autocomplete = new google.maps.places.Autocomplete((document.getElementById('autocomplete')),{types: ['geocode']});
 
   autocomplete.addListener('place_changed', function(){
@@ -20,7 +19,6 @@ function initautocomplete() {
   localStorage.setItem('longitude',lng);
 
   });
-  
 }
 
 function geolocate() {
@@ -38,17 +36,7 @@ function geolocate() {
     });
   }
 }
-/*
-* author - Kennouche Omar Wild Code School Session 1
-*/
 
-// On stock dans une variable les valeurs de l'address qui provient du formulaire de recherche de la homepage
-address = localStorage.getItem('address');
-// On stock dans une variable les valeurs de latitude qui provient du formulaire de recherche de la homepage
-latitudeSearch = parseFloat(localStorage.getItem('latitude'));
-// On stock dans une variable les valeurs de longitude qui provient du formulaire de recherche de la homepage
-longitudeSearch = parseFloat(localStorage.getItem('longitude'));
-// declaration de la fonction initMap qui sera appeler en callback dans l'url de l'api google map
 /***************************************************
 * author - Kennouche Omar Wild Code School Session 1
 ****************************************************/
@@ -60,15 +48,13 @@ var marker_path;
 var map;
 // On stock dans une variable les valeurs de l'address qui provient du formulaire de recherche de la homepage.
 address = localStorage.getItem('address');
+choice = localStorage.getItem('choice');
 // On stock dans une variable les valeurs de latitude qui provient du formulaire de recherche de la homepage.
 latitudeSearch = parseFloat(localStorage.getItem('latitude'));
 // On stock dans une variable les valeurs de longitude qui provient du formulaire de recherche de la homepage.
 longitudeSearch = parseFloat(localStorage.getItem('longitude'));
 // declaration de la fonction initMap qui sera appeler en callback dans l'url de l'api google map.  
-
 function initMap() {
-  
-  marker_path = "http://localhost/Tobook-Chartres/web/img/marker_map/tobook.png"
 
   var myLatLng = {lat: latitudeSearch, lng: longitudeSearch};
 
@@ -79,7 +65,7 @@ function initMap() {
           mapTypeControl: true,
           mapTypeControlOptions: {
           style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-          position: google.maps.ControlPosition.TOP_CENTER
+          position: google.maps.ControlPosition.TOP_CENTER  
           },
           streetViewControl: true,
           streetViewControlOptions: {
@@ -91,15 +77,52 @@ function initMap() {
 
   var service = new google.maps.places.PlacesService(map);
 
-  service.nearbySearch({
-         location: myLatLng,
-         radius: 10000,
-         types: ['lodging']
-         }, callback);
+  if(choice == 'hotels'){
+      marker_path = "http://localhost/Chartres-ToBook/web/img/marker_map/hotels.png";
+      service.nearbySearch({
+           location: myLatLng,
+           radius: 10000,
+           types: ['lodging']
+           }, callback);
+
+    }else if(choice == 'chambreHotes'){
+      marker_path = "http://localhost/Chartres-ToBook/web/img/marker_map/hotels.png";
+      service.nearbySearch({
+           location: myLatLng,
+           radius: 10000,
+           types: ['lodging']
+           }, callback);
+
+    }else if(choice == 'gites'){
+      marker_path = "http://localhost/Chartres-ToBook/web/img/marker_map/hotels.png";
+      service.nearbySearch({
+           location: myLatLng,
+           radius: 10000,
+           types: ['lodging']
+           }, callback);
+
+    }else if(choice == 'restaurants'){
+      marker_path = "http://localhost/Chartres-ToBook/web/img/marker_map/restaurant64.png";
+      service.nearbySearch({
+           location: myLatLng,
+           radius: 10000,
+           types: ['restaurant']
+           }, callback);
+
+    }else if(choice == 'musees'){
+      marker_path = "http://localhost/Chartres-ToBook/web/img/marker_map/museum64.png";
+      service.nearbySearch({
+           location: myLatLng,
+           radius: 10000,
+           types: ['museum']
+           }, callback);
+    }
 }
 
 function callback(results, status) {
   if (status === google.maps.places.PlacesServiceStatus.OK) {
+    var resultat = JSON.stringify(results);
+    localStorage.setItem('resultat',resultat);
     for (var i = 0; i < results.length; i++) {
       createMarker(results[i]);
     }
@@ -123,7 +146,57 @@ function createMarker(place) {
   //****//
   marker.addListener('click', function() {
   infowindow.open(map, marker);
-  initialize();
   });
   
 }
+ 
+// function initMap() {
+
+//     var map;
+//     var myLatLng = {lat: latitudeSearch, lng: longitudeSearch};
+//     //****//
+//     map = new google.maps.Map(document.getElementById('map'), {
+//           center: myLatLng,
+//           zoom: 14,
+//           zoomControl: true,
+//           mapTypeControl: true,
+//           mapTypeControlOptions: {
+//          style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+//          position: google.maps.ControlPosition.TOP_CENTER
+//        },
+//          streetViewControl: true,
+//          streetViewControlOptions: {
+//          position: google.maps.ControlPosition.LEFT_TOP
+//          }
+//     });
+
+//     //****//
+//    var marker = new google.maps.Marker({
+//      map: map,
+//      animation: google.maps.Animation.DROP,
+//      position: myLatLng,
+//      title: address
+//    });
+//    //****//
+//    var contentString = "<div id='street-view' style='width:340px;min-height:160px;'></div>";
+//    //****//
+//    var infowindow = new google.maps.InfoWindow({
+//      content: contentString,
+//    });
+//    //****//
+//    marker.addListener('click', function() {
+//      infowindow.open(map, marker);
+//      initialize();
+//    });
+// }
+
+// var panorama;
+// function initialize() {
+//   panorama = new google.maps.StreetViewPanorama(
+//       document.getElementById('street-view'),
+//       {
+//         position: {lat: latitudeSearch, lng: longitudeSearch},
+//         pov: {heading: 165, pitch: 0},
+//         zoom: 1
+//       });
+// }
