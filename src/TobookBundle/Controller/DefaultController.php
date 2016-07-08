2ec4db7885f8e633e2d9c10d8e31f69217b2775d
PTony <jokr.01@gmail.com>
2016-07-08 10:35:59 +0200

    maj vue resultat de recherche

--- src/TobookBundle/Controller/DefaultController.php
+++ src/TobookBundle/Controller/DefaultController.php
@@ -39,6 +39,68 @@ class DefaultController extends Controller
         $prix       = $request->query->get('prix');
         $etoiles    = $request->query->get('etoiles');
         $note       = $request->query->get('note');
+
+        $d = $this->getDoctrine()->getRepository('WCSPropertyBundle:Professionnel')->createQueryBuilder('p');
+        $d
+            ->select('p')
+            ->addSelect(
+            '( 6371 * acos(cos(radians( :latitude )) * cos( radians( p.profLatitude ) ) * cos( radians( p.profLongitude ) - radians( :longitude) ) + sin( radians( :latitude ) ) * sin( radians( p.profLatitude ) ) ) ) as distance'
+        )   ->where('p.profActif = :enabled')
+            ->having('distance < :distance')
+            ->orderBy('distance', 'ASC')
+            ->setFirstResult(0)  
+            ->setMaxResults(15)
+            ->setParameter('latitude', $latitude)
+            ->setParameter('longitude', $longitude)
+            ->setParameter('distance', $distance)
+            ->setParameter('enabled', 1);
+        $query= $d->getQuery();
+        $resultats= $query->getResult();
+
+        // var_dump($resultats);
+
+        $tab_resultats = [];
+        if (!empty($resultats))
+        {
+            foreach ($resultats as $res)
+            {   
+                $tab_res = [];
+                $tab_res[] = array(
+                        'distance'=>$res['distance'],
+                        'id'    =>$res[0]->getProfId(),
+                        'lat'   =>$res[0]->getProfLatitude(),
+                        'lng'   =>$res[0]->getProfLongitude(),
+                        'lng'   =>$res[0]->getProfLongitude(),
+                        'profNom'           =>$res[0]->getProfNom(),
+                        'profDescriptif'    =>$res[0]->getProfDescriptif(),
+                        'profPrixMini'      =>$res[0]->getProfPrixMini(),
+                        'profEtoiles'       =>$res[0]->getProfEtoiles(),
+                    );
+                array_push($tab_resultats, $tab_res);
+            }
+        }        
+
+
+        return $this->render('TobookBundle:Default:search.html.twig', array(
+            'base_dir'  => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'latitude'  => $latitude,
+            'longitude' => $longitude,
+            'address'    => $address,
+            'resultats' => $tab_resultats,
+        ));
+    }
+
+public function jsonNearbyAction(Request $request)
+    {   
+
+        $address    = $request->query->get('address');
+        $latitude   = $request->query->get('latitude');
+        $longitude  = $request->query->get('longitude');
+        $distance   = 10.5;
+
+        $prix       = $request->query->get('prix');
+        $etoiles    = $request->query->get('etoiles');
+        $note       = $request->query->get('note');
         // var_dump($address);
         // var_dump($latitude);
         // var_dump($longitude);
@@ -68,18 +130,20 @@ class DefaultController extends Controller
         //         break;
         // }
 
-        $d = $this->getDoctrine()->getRepository('WCSPropertyBundle:Professionnel')->createQueryBuilder('l');
+        $d = $this->getDoctrine()->getRepository('WCSPropertyBundle:Professionnel')->createQueryBuilder('p');
         $d
-            ->select('l')
+            ->select('p')
             ->addSelect(
-            '( 6371 * acos(cos(radians(' . $latitude . ')) * cos( radians( l.profLatitude ) ) * cos( radians( l.profLongitude ) - radians(' . $longitude . ') ) + sin( radians(' . $latitude . ') ) * sin( radians( l.profLatitude ) ) ) ) as distance'
-        )   ->where('l.profActif = :enabled')
+            '( 6371 * acos(cos(radians( :latitude )) * cos( radians( p.profLatitude ) ) * cos( radians( p.profLongitude ) - radians( :longitude) ) + sin( radians( :latitude ) ) * sin( radians( p.profLatitude ) ) ) ) as distance'
+        )   ->where('p.profActif = :enabled')
             ->having('distance < :distance')
             ->orderBy('distance', 'ASC')
             ->setFirstResult(0)  
-            ->setMaxResults(2)
-            ->setParameter('enabled', 1)
-            ->setParameter('distance', $distance);
+            ->setMaxResults(15)
+            ->setParameter('latitude', $latitude)
+            ->setParameter('longitude', $longitude)
+            ->setParameter('distance', $distance)
+            ->setParameter('enabled', 1);
         $query= $d->getQuery();
         $resultats= $query->getResult();
 
@@ -92,10 +156,16 @@ class DefaultController extends Controller
             {   
                 $tab_res = [];
                 $tab_res[] = array(
-                        'id'=>$res[0]->getProfId(),
                         'distance'=>$res['distance'],
-                        'lat'=>$res[0]->getProfLatitude(),
-                        'lng'=>$res[0]->getProfLongitude(),
+                        'id'    =>$res[0]->getProfId(),
+                        'lat'   =>$res[0]->getProfLatitude(),
+                        'lng'   =>$res[0]->getProfLongitude(),
+                        'lng'   =>$res[0]->getProfLongitude(),
+                        'profNom'           =>$res[0]->getProfNom(),
+                        'profDescriptif'    =>$res[0]->getProfDescriptif(),
+                        'profPrixMini'      =>$res[0]->getProfPrixMini(),
+                        'profEtoiles'       =>$res[0]->getProfEtoiles(),
+
                     );
                 array_push($tab_resultats, $tab_res);
             }
@@ -113,7 +183,7 @@ class DefaultController extends Controller
             'latitude'  => $latitude,
             'longitude' => $longitude,
             'address'    => $address,
-            'resultats' => $resultats,
+            'resultats' => $tab_resultats,
         ));
     } 
 }
